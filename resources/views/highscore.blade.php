1eba929c2a42e40f05059670f422a6c6fce32513
Jupiterlander <jupiterlander@YOGA-9.localdomain>
2021-05-19 09:13:12 +0200

    New Highscore view

---
+++ resources/views/highscore.blade.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+@extends('layout')
+
+@section('content')
+
+<nav>
+    <ul class="breadcrumb">
+                    <li>
+                        <a href="{{ url('/') }}">Home</a>
+                    </li>
+                    <li>
+                        <a href="{{ url('/yatzy') }}">Yatzy</a>
+                    </li>
+                    <li>
+                        Highscore
+                    </li>
+    </ul>
+</nav>
+
+
+<div>
+    <table class="highscore">
+        <tr>
+            <th colspan=3 style="background: lightgray;">Highscore</th>
+        </tr>
+
+        @foreach ($highscores as $score)
+        @php
+            ;
+        @endphp
+        <tr>
+            <td style="width: 5rem;">{{ $score->akronym }}</td>
+            <td style="width: 5rem;">{{ $score->score }}</td>
+            <td>{{ $score->updated_at }}</td>
+        </tr>
+        @endforeach
+    </table>
+</div>
+
+@endsection
