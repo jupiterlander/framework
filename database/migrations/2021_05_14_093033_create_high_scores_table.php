fea74c58997b8ad52daec49995da9e6f7bb868a8
Jupiterlander <jupiterlander@YOGA-9.localdomain>
2021-05-19 09:12:18 +0200

    New Highscore migration

---
+++ database/migrations/2021_05_14_093033_create_high_scores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHighScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('high_scores', function (Blueprint $table) {
+            $table->id();
+            $table->string('akronym', 3)->nullable($value = true);
+            $table->smallInteger('score');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('high_scores');
+    }
+}
