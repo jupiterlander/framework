476674f4a92e2168b80e3ee11bc5f8d711af36dc
Jupiterlander <jupiterlander@YOGA-9.localdomain>
2021-05-28 14:07:38 +0200

    Minor change for scrutinizer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Schema; // for scrutinizer
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // for scrutinizer
+        Schema::defaultStringLength(191);
     }
 }
